299639250f779bc3db309fbde871a72c27f9608a
Shahbaaz <Shahbaz.Chaviwale.Contractor@pepsico.com>
2026-02-26 01:07:59 +0530

    first init commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: { ecmaVersion: 2020, sourceType: "module" },
+  settings: { react: { version: "detect" } },
+};
